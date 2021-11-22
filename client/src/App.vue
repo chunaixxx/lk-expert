8f709a7c8a77f6df9d68a0c1b4763d6680a86312
VashUber <mrjarokvk@gmail.com>
2021-11-22 21:54:01 +0300

    added vue config file

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,19 @@
 <template>
-  <div class="app"></div>
+  <div class="app">
+    <Button text="button" />
+  </div>
 </template>
+
+<script>
+import Button from "@/components/Button.vue";
+
+export default {
+  name: "App",
+  components: {
+    Button,
+  },
+};
+</script>
 
 <style lang="scss">
 </style>
---
+++ client/src/components/Button.vue
@@ -0,0 +1,24 @@
+<template>
+  <button class="button">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.button {
+  padding: 15px 24px;
+  font-size: 18px;
+  background: $red;
+  border: none;
+  outline: none;
+  color: $white;
+  min-width: 184px;
+}
+</style>
