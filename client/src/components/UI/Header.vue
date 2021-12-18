132c0641637cbb077165d236a4ec376bd808a67e
VashUber <mrjarokvk@gmail.com>
2021-12-19 00:49:25 +0300

    add prettierrc

--- client/src/components/UI/Header.vue
+++ client/src/components/UI/Header.vue
@@ -1,22 +1,22 @@
 <template>
-  <header class="header">
-    <img src="@/assets/logo.svg" alt="" />
-    <img src="@/assets/logo2.svg" alt="" />
+	<header class="header">
+		<img src="@/assets/logo.svg" alt="" />
+		<img src="@/assets/logo2.svg" alt="" />
 
-    <h1 class="header__title">{{ props.title }}</h1>
-  </header>
+		<h1 class="header__title">{{ props.title }}</h1>
+	</header>
 </template>
 <script setup>
-  import { defineProps } from "vue"
+import { defineProps } from 'vue'
 
-  const props = defineProps({ title: String })
+const props = defineProps({ title: String })
 </script>
 <style scoped lang="scss">
-  .header {
-    background: $backgroundHeader;
+.header {
+	background: $backgroundHeader;
 
-    &__title {
-      color: #fff;
-    }
-  }
+	&__title {
+		color: #fff;
+	}
+}
 </style>
