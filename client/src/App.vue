39228ec6bd4575f00b7ec31e02c5811225600206
VashUber <mrjarokvk@gmail.com>
2021-12-19 00:34:38 +0300

    prettier

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,12 @@
 <template>
   <div class="app">
-    <VButton class="button_primary-outline-600"> Самара </VButton>
-    <VButton class="button_primary-600"> Самара </VButton>
-    <VButton class="button_secondary"> Самара </VButton>
-    <VButton class="button_primary-400"> Самара </VButton>
-    <VInput placeholder="Абоба"/>
-    <img src="./assets/logo.svg" alt="">
-    <img src="./assets/logo2.svg" alt="">
+    <Header title="Test Test" />
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+  import Header from "@/components/UI/Header.vue"
+</script>
 
 <style lang="scss">
   @import url("https://fonts.googleapis.com/css2?family=Roboto&display=swap");
---
+++ client/src/components/UI/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="header">
+    <img src="@/assets/logo.svg" alt="" />
+    <img src="@/assets/logo2.svg" alt="" />
+
+    <h1 class="header__title">{{ props.title }}</h1>
+  </header>
+</template>
+<script setup>
+  import { defineProps } from "vue"
+
+  const props = defineProps({ title: String })
+</script>
+<style scoped lang="scss">
+  .header {
+    background: $backgroundHeader;
+
+    &__title {
+      color: #fff;
+    }
+  }
+</style>
